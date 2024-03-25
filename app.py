e6884ac31832a0ceb6d7ee90c6b2020ddd51a9e1
mariewoergoetter <161148962+mariewoergoetter@users.noreply.github.com>
2024-03-25 09:30:16 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from text_feature_transformer import TextFeatureTransformer
+from joblib import load
+
+# Load your trained model
+model_path = '/Users/mlwfee/Desktop/CEFR_Classifier/xgboost_model.joblib'
+model = load(model_path)
+
+# Define the mapping from numeric predictions back to CEFR levels
+# This should match the encoding used during training
+cefr_mapping = {
+    0: 'A1',
+    1: 'A2',
+    2: 'B1',
+    3: 'B2',
+    4: 'C1',
+    5: 'C2'
+}
+
+def predict(text):
+    # This function makes a prediction based on the input text.
+    prediction = model.predict([text])
+    # Use the mapping to convert numeric prediction back to CEFR level
+    cefr_level = cefr_mapping[prediction[0]]
+    return cefr_level
+
+# Streamlit app
+st.title('CEFR Level Classifier')
+
+# User text input
+user_input = st.text_area("Enter the text you want to have assessed here:", "")
+
+if st.button('Predict'):
+    # Display the prediction
+    prediction = predict(user_input)
+    st.write(f'Your level is probably: {prediction}')
+
