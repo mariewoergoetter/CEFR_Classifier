544a018fcae76b8a6b9c069874ee16688c798d3c
mariewoergoetter <161148962+mariewoergoetter@users.noreply.github.com>
2024-03-26 15:16:00 +0100

    Update model_dump.py

--- model_dump.py
+++ model_dump.py
@@ -1,10 +1,7 @@
 from joblib import dump
 
-# Assuming your last trained pipeline is in a variable named 'pipeline'
-# Define the path and filename for saving the model
-model_filename = '/Users/mlwfee/Desktop/CEFR_Classifier/xgboost_model.joblib'
+model_filename = 'xgboost_model.joblib'
 
-# Save the model
 dump(pipeline, model_filename)
 
 print(f"Model saved to {model_filename}")
